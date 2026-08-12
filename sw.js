024cee97bbab25628e449f776b98044d48f34588
yazelin <yazelin@ching-tech.com>
2026-08-12 18:09:57 +0800

    chore: sw 版號重算

--- sw.js
+++ sw.js
@@ -8,7 +8,7 @@
  * SHELL_V 由 tools/build_sw.py 依 precache 清單的內容 hash 產生，不要手改。
  * 手動 bump 的遲早會忘記，而忘記的症狀是「使用者永遠看不到新版」，沒有任何徵兆。
  */
-const SHELL_V = "shell-1c50e12";
+const SHELL_V = "shell-e6a6267";
 const ASSET_V = "asset-v1";
 
 // 前綴要跨專案唯一。CacheStorage 是 per-origin，yazelin.github.io 底下所有專案
@@ -79,7 +79,7 @@ const ASSET_LIST = [
   "img/pingtung-paiwan-base.webp",
   "img/yilan-kavalan-base.webp",
   "img/hsinchu-county-saysiyat-base.webp",
-  "img/hsinchu-county-tayal-base.webp",
+  "img/miaoli-tayal-base.webp",
   "img/chiayi-county-cou-base.webp",
   "audio/theme.mp3",
 ];
